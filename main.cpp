e0ed4f1df291c79332efec81974fab317a279c7f
Tim <mail.tim.heinrichs@gmail.com>
2024-07-10 13:41:18 +0200

    Das Programm funktioniert nun, und findet beliebige Ausschnitte, sofern sie Strickt aus dem Bild sind. Da OpenCv mal in X/Y und mal in Y/X Achsen arbeitet, habe Ich einige Variablen vertauscht und diese nun korregiert.

--- main.cpp
+++ main.cpp
@@ -62,9 +62,10 @@ using namespace cv;
     int templatematching (int threshold, Mat & origin, Mat & excerpt , int ii, int jj)
     {
 
-    int ie = 0;
-    int je = 0;
-    int hits = 0;
+    int ie        = 0;
+    int je        = 0;
+    int hits      = 0;
+    int total_dif = 0;
 
     int width_origin = origin.cols;
     int height_origin = origin.rows;
@@ -85,9 +86,6 @@ using namespace cv;
 
         int dif = 0;
 
-         //cout<< " Je:" << je << " / " << height_excerpt << " ";
-
-
             while (ie < width_excerpt )
             {
                 if( ( (width_excerpt + ie + 1) > width_origin) )
@@ -96,20 +94,18 @@ using namespace cv;
                     break;
                 }
 
-                //cout<< " Ie:" << ie << " / " << width_excerpt << endl;
-                //cout << "\n  Threshold:  " << threshold << "      \n" ;
-                origin_at = origin.at<uchar>(   (jj + je),(ii + ie)  );
+                origin_at = origin.at<uchar>((jj + je),(ii + ie));
                 excerpt_at = excerpt.at<uchar> (je, ie);
-                dif = difference(origin_at, excerpt_at);
+                dif = difference(origin_at,excerpt_at);
 
                 //cout << " dif: " << dif << "  " ;
 
+
                 if (dif <= threshold)
                 {
+                    total_dif += dif;
                    hits++;
                 }
-
-
 
                 //cout << "Hits Ie :" << hits << "     " << endl;
                 ie++;
@@ -119,8 +115,9 @@ using namespace cv;
     }
 
     if(hits == (width_excerpt*height_excerpt)) cout << "Bildausschnitt im Bild gefunden!\n"<< endl;
-    return hits;
-}
+    //return hits;
+    return total_dif;
+    }
 
 
         Point_<int> location (int threshold , Mat& origin, Mat& excerpt)
@@ -141,38 +138,46 @@ using namespace cv;
         waitKey(0);
 
     int most_hits = 0;
+    int min_dif = 100000 ;
     Point_<int> most_hits_point = Point_(0,0);
     int w = (origin_bw.cols - 1 );
     int h = (origin_bw.rows - 1 );
     int we = (excerpt_bw.cols - 1 );
     int he = (excerpt_bw.rows - 1 );
-    //cout << "w h we he" << w << h << we << he << endl;
 
     for (int i = 0 ; i < ((w-we-1)) ; i++)
     {
-        //cout << origin_bw.cols << " / "<< origin_bw.rows << endl;
-        //Test einer "Progress-Bar"
+        //Eine "Progress-Bar"
         if (i % (w/100) == 0)
         {
-            cout << (i / (w/100)) << "%" << endl;
+            cout << (i / ((w-we)/100)) << "%" << endl;
         }
 
-        for (int j = 0; j < (((h-he)-1)); j++) {
+        for (int j = 0; j < (((h-he)-1)/2); j++) {
             //cout << " J: " << j << "  / " << h <<"/" << he <<endl;
 
-            int hits = templatematching(threshold,origin_bw,excerpt_bw,i,j);
+            int hits = templatematching(threshold,origin_bw,excerpt_bw,i,j*2);
 
             //cout << " Hits: " << hits << " " << endl ;
 
-            if (hits == (origin_bw.cols*origin_bw.rows))break;
+            if (hits == (origin_bw.cols*origin_bw.rows)) break;
 
-            if (hits > most_hits)
+            if (min_dif > hits)
             {
-                most_hits=hits;
-                most_hits_point = Point_( (i + (excerpt_bw.cols/2)),(j+(excerpt_bw.rows/2)));
-                cout << "Hits:" << most_hits << " at :" << most_hits_point << endl;
+                min_dif = hits;
+                most_hits_point = Point_( (i + (excerpt_bw.cols/2)),(j*2+(excerpt_bw.rows/2)) );
+                cout << "Dif: " << min_dif << " at : " << most_hits_point << endl ;
+
             }
 
+
+            /* if (hits > most_hits)
+            {
+                most_hits=hits;
+                most_hits_point = Point_( (i + (excerpt_bw.cols/2)),(j*2+(excerpt_bw.rows/2)));
+                cout << "Hits:" << most_hits << " at :" << most_hits_point << endl;
+            }
+            */
         }
 
     }
@@ -187,13 +192,14 @@ int main()
 
     cv::Mat testbild = cv::imread("tmp2.jpg");
     cv::Mat ausschnitt = cv::imread("tmpausschnitt3.jpg");
+    cv::Mat ausschnitt2 = cv::imread("tmpausschnitt32.jpg");
     cv::imshow("fenster", testbild);
     cv::waitKey(10);
 
     //cv::imshow("Ausschitt",ausschnitt);
     //waitKey(0);
 
-    Point_<int> result = location( 10 ,testbild,ausschnitt);
+    Point_<int> result = location( 30 ,testbild,ausschnitt);
     cout << result << endl;
 
     if(result != Point_<int>(0,0))
@@ -202,15 +208,20 @@ int main()
         circle(testbild,result,50,Scalar_(0,0,255),4);
         circle(testbild,result,5,Scalar_(0,0,255),1);
 
-
         cout << result << endl;
 
         cv::imshow("fenster2",  testbild); //Test test test i love uuu <3 work weiter bitch
-        cv::waitKey(0);
-        cv::waitKey(0);
-        cv::waitKey(0);
+
+        cv::waitKey(10000);
+
+        result = location(30,testbild,ausschnitt2);
+        circle(testbild,result,50,Scalar_(0,0,255),4);
+        circle(testbild,result,5,Scalar_(0,0,255),1);
 
 
+        cv::imshow("fenster3",testbild);
+        cv::waitKey(0);
+        cv::waitKey(0);
     }
     else
     {
@@ -218,6 +229,7 @@ int main()
         return 1;
     }
 
-    return 0;
 
+
+    return 0;
     }
